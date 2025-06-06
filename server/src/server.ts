d42290b3e52bc115aac4a6a9f453c851cb9c8607
Stephen Schier <stephenjschier@gmail.com>
2025-06-06 15:50:50 -0500

    minor change to server.ts and import sequelize

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,7 @@ dotenv.config({ path:'../.env'  });
 
 import express from 'express';
 import routes from './routes/index.js';
-import { sequelize } from './models/index.js';
+import sequelize from './models/index.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
